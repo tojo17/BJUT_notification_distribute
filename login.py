eb60d256ebfaf18911cf7fe5f1df0c2860233b91
SykieChen <chen17@coder17.com>
2018-03-05 04:57:07 +0800

    fixed loop

--- login.py
+++ login.py
@@ -8,13 +8,14 @@ import sys
 import time
 import json
 
-debug = False
+debug = True
 def retry_get(retry, session, h_url, **kwargs):
 	ctTry = 0
+	global debug
 	while 1:
 		try:
 			print_debug("retry_get", ctTry)
-			res = session.get(h_url, timeout=30, **kwargs)
+			res = session.get(h_url, timeout=30, verify = debug, **kwargs)
 		except:
 			if ctTry < retry:
 				ctTry += 1
@@ -31,10 +32,11 @@ def retry_get(retry, session, h_url, **kwargs):
 
 def retry_post(retry, session, h_url, **kwargs):
 	ctTry = 0
+	global debug
 	while 1:
 		try:
 			print_debug("retry_post", ctTry)
-			res = session.post(h_url, timeout=30, **kwargs)
+			res = session.post(h_url, timeout=30, verify = debug, **kwargs)
 		except:
 			if ctTry < retry:
 				ctTry += 1
@@ -59,12 +61,39 @@ def print_debug(*text):
 	if debug: print_log(*text)
 	return
 
+def init_db():
+    global conn
+    conn = sqlite3.connect("webdata.db")
+    log_out("DB link success.")
+    c = conn.cursor()
+    if not ex_append:
+        data = c.execute('SELECT name FROM sqlite_master;')
+        for row in data:
+            print(row)
+            if (row[0] != 'sqlite_sequence'):
+                c.execute('DROP TABLE %s;' %row[0])
+            else:
+                c.execute('DELETE FROM %s;' %row[0])
+
+        c.execute('''
+            CREATE TABLE sites (
+                id          INTEGER NOT NULL    PRIMARY KEY AUTOINCREMENT   UNIQUE,
+                name        TEXT    NOT NULL,
+                url         TEXT,
+                country     TEXT,
+                category    TEXT,
+                description TEXT
+            );
+        ''')
+        conn.commit()
+
 def login(user, password):
+	global session
 	# get lt and exe
 	url = 'https://cas.bjut.edu.cn/login'
 	print_log("Logging in...")
 	session = requests.session()
-	res = retry_get(30, session, url, verify = False)
+	res = retry_get(30, session, url)
 	etr = etree.HTML(res.text)
 	lt = etr.xpath('////div[@class="form-group  dl-btn"]/input[@name="lt"]/@value')[0]
 	execution = etr.xpath('////div[@class="form-group  dl-btn"]/input[@name="execution"]/@value')[0]
@@ -77,15 +106,16 @@ def login(user, password):
 		'_eventId':	'submit',
 		'submit':	''
 	}
-	res = retry_post(30, session, url, data = data, verify = False)
+	res = retry_post(30, session, url, data = data)
 	if '<div id="msg" class="success">' in res.text:
 		print_log('Login success!')
-		return session
+		return True
 	else:
 		print_log('Login failed!')
-		return None
+		return False
 
-def get_institute_noti(session):
+def get_institute_noti():
+	global session
 	url = 'https://my1.bjut.edu.cn/group/undergraduate/index'
 	params = {
 		'p_p_id':	'bulletinListForCustom_WAR_infoDiffusionV2portlet_INSTANCE_O5zYIiq6Mmwb',
@@ -97,15 +127,15 @@ def get_institute_noti(session):
 		'iDisplayLength': 10,
 		'sSearch': ''
 	}
-	json = retry_get(30, session, url, params = params, verify = False).text
+	json = retry_get(30, session, url, params = params).text
 	return json
 
 if __name__ == '__main__':
 	# wait for another self to exit
 	# time.sleep(20)
-	while session == None:
-		session = login('15071025', 'cixi19344243')
-	json = get_institute_noti(session)
+	while not login('15071025', 'cixi19344243'):
+		pass
+	json = get_institute_noti()
 
 
 
